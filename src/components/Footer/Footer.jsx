72a247cb9168f0dc54dfa7db393fd5a74ffee013
kapatric <khristopherpatrick@gmail.com>
2023-04-27 10:56:06 -0400

    inline

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import "./Footer.css";
 
 export default function Footer() {
-  
+
+return (
+  <div>
+    Little Lemon Restuarant © 2023. All rights reserved.
+  </div>
+)
 }
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -6,8 +6,8 @@ export default function Nav() {
 
 return (
   <nav>
-    <img src="little lemon.png" alt="" />
     <ul>
+    <li><img src="little lemon.png" alt="" width="175" height="125"/></li>
       <li><a href="/" className="home">Home</a></li>
       <li><a href="" className="about">About</a></li>
       <li><a href="" className="menu">Menu</a></li>
