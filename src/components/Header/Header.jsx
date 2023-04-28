d16ca9f2308ee56775ceb208a8226c1c38e3e176
kapatric <khristopherpatrick@gmail.com>
2023-04-28 12:16:00 -0400

    creation of reserve button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import "./Header.css"
 
 export default function Header() {
-  
+  return (
+    <header>
+      <div className="btn">
+        <button>Reserve A Table</button>
+      </div>
+    </header>
+  )
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import "./Main.css" 
 
 export default function Main() {
   
+  return (
+    <div className="container">
+      <div className="grid-item">pop</div>
+      <div className="grid-item">pop</div>
+      <div className="grid-item">pop</div>
+  </div>
+)
 }
