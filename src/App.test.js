4a395da18a08b27b751bbd21c5bd66360a199a3e
kapatric <khristopherpatrick@gmail.com>
2023-05-08 08:23:16 -0400

    writing unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Reserve from './components/Reserve/Reserve';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+// test('Renders Reserve text', () => {
+//   render(<Reserve />);
+//   const h3 = screen.getByText("Email:");
+//   expect(h3).toBeInTheDocument();
+// })
+
+test('Renders time stamps', () => {
+  render(<Reserve />);
+  const initializeTimes = screen.getByDisplayValue("1:00");
+  expect(initializeTimes).toBeInTheDocument();
+})
