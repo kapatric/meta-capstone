ad7cf437b79ba730b78e6f4fa0a78260e25ac7ca
kapatric <khristopherpatrick@gmail.com>
2023-05-09 10:39:37 -0400

    adding options

--- src/components/Reserve/Reserve.jsx
+++ src/components/Reserve/Reserve.jsx
@@ -1,12 +1,11 @@
-import React, {useState, useRef} from "react";
+import React, { useState, useRef } from "react";
 import "./Reserve.css";
 
 export default function Reserve() {
-  const [date, setDate] = useState('');
-  const [time, setTime] = useState('');
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
   const dateInputRef = useRef(null);
   const timeInputRef = useRef(null);
-  
 
   const handleChange = (e) => {
     setDate(e.target.value);
@@ -18,30 +17,35 @@ export default function Reserve() {
 
   return (
     <center>
-    <form className="reserve">
-      <h3>Make a Resevation</h3>
+      <form className="reserve">
+        <h3>Make a Resevation</h3>
         <label for="date">Date: {date}</label>
-        <input type="date" id="date" onChange={handleChange} ref={dateInputRef } />
+        <input
+          type="date"
+          id="date"
+          onChange={handleChange}
+          ref={dateInputRef}
+        />
         <br></br>
         <br></br>
         <label for="res_time">Time: {time} </label>
-      <select id="res_time" onChange={handleTime} ref={timeInputRef}>
-        <option>11:00</option>
-        <option>12:00</option>
-        <option>1:00</option>
-        <option>2:00</option>
-        <option>3:00</option>
-        <option>4:00</option>
-        <option>5:00</option>
-        <option>6:00</option>
-        <option>7:00</option>
-        <option>8:00</option>
-        <option>9:00</option>
-        <option>10:00</option>
-      </select>
+        <select id="res_time" onChange={handleTime} ref={timeInputRef}>
+          <option>11:00</option>
+          <option>12:00</option>
+          <option>1:00</option>
+          <option>2:00</option>
+          <option>3:00</option>
+          <option>4:00</option>
+          <option>5:00</option>
+          <option>6:00</option>
+          <option>7:00</option>
+          <option>8:00</option>
+          <option>9:00</option>
+          <option>10:00</option>
+        </select>
         <br></br>
         <br></br>
-      <label for="guests">Party Size: </label> 
+        <label for="guests">Party Size: </label>
         <input
           type="number"
           placeholder="1"
@@ -52,24 +56,32 @@ export default function Reserve() {
         <br></br>
         <br></br>
         <label for="occasion">Occasion: </label>
-      <select id="occasion">
-        <option>Birthday</option>
-        <option>Anniversary</option>
-        <option>Engagement</option>
-        <option>Retirement</option>
+        <select id="occasion">
+          <option>Birthday</option>
+          <option>Anniversary</option>
+          <option>Engagement</option>
+          <option>Retirement</option>
+          <option>Get Together</option>
+          <option>Just Dinner</option>
         </select>
         <br></br>
         <br></br>
         <label for="phone-number">Phone: </label>
-        <input type="number" id="phone_number" name="phone_number" minLength={10} maxLength={10} />
+        <input
+          type="number"
+          id="phone_number"
+          name="phone_number"
+          minLength={10}
+          maxLength={10}
+        />
         <label for="user_email">Email: </label>
-       <input type="email" id="user_email" name="user_email"/>
-      <br></br>
-      <br></br>
-      <center>
-       <input className="submit" type="submit" value="Make a reservation" />
-      </center>
+        <input type="email" id="user_email" name="user_email" />
+        <br></br>
+        <br></br>
+        <center>
+          <input className="submit" type="submit" value="Make a reservation" />
+        </center>
       </form>
-      </center>
+    </center>
   );
 }
